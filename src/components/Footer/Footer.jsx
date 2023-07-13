ba67abffde555e9adaadf746e1de61d1dcfc2721
vulebaolong <vulebaolong@gmail.com>
2023-07-13 23:06:47 +0700

    fix hover tag a footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,41 +8,43 @@ function Footer() {
 		<footer className={`${style.content_footer} pt-28`}>
 			<div className="container">
 				<div className="md:flex md:justify-between">
-					<Logo />
+					<div className="">
+						<Logo />
+					</div>
 					<div
 						className="grid 
 						mt-16 gap-10 grid-cols-1 
 						sm:mt-16 sm:gap-10 sm:grid-cols-2 
 						md:mt-0
-						lg:grid-cols-3 
+						lg:grid-cols-3
+						xl:gap-20
 						"
-						// gap-20
 					>
 						<div>
 							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Trợ giúp</h2>
 							<ul className="text-gray-500 dark:text-gray-500 font-medium space-y-2">
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/node/412" className="dark:hover:text-gray-200 ">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/node/412" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Các câu hỏi thường gặp
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://media.netflix.com/en/" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://media.netflix.com/en/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Trung tâm đa phương tiện
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://devices.netflix.com/en/" className=" dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://devices.netflix.com/en/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Các cách xem
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="/" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Tuỳ chọn cookie
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://fast.com/" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://fast.com/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Kiểm tra tốc độ
 									</a>
 								</li>
@@ -52,27 +54,37 @@ function Footer() {
 							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Thông tin</h2>
 							<ul className="text-gray-500 dark:text-gray-500 font-medium space-y-2">
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Trung tâm trợ giúp
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://ir.netflix.net/ir-overview/profile/default.aspx" className="dark:hover:text-gray-200">
+									<a
+										target="_blank"
+										rel="noreferrer"
+										href="https://ir.netflix.net/ir-overview/profile/default.aspx"
+										className={`dark:hover:text-gray-200 ${style.footer_item}`}
+									>
 										Quan hệ với nhà đầu tư
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/termsofuse" className="dark:hover:text-gray-200 ">
+									<a
+										target="_blank"
+										rel="noreferrer"
+										href="https://help.netflix.com/legal/termsofuse"
+										className={`dark:hover:text-gray-200 ${style.footer_item}`}
+									>
 										Điều khoản sử dụng
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/corpinfo" className=" dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/corpinfo" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Thông tin doanh nghiệp
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/notices" className="dark:hover:text-gray-200 ">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/notices" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Thông báo pháp lý
 									</a>
 								</li>
@@ -82,27 +94,27 @@ function Footer() {
 							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Kết nối</h2>
 							<ul className="text-gray-500 dark:text-gray-500 font-medium space-y-2">
 								<li className="">
-									<NavLink to="/login" className="dark:hover:text-gray-200">
+									<NavLink to="/login" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Tài khoản
 									</NavLink>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://jobs.netflix.com/" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://jobs.netflix.com/" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Việc làm
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/privacy" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/legal/privacy" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Quyền riêng tư
 									</a>
 								</li>
 								<li>
-									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/contactus" className="dark:hover:text-gray-200">
+									<a target="_blank" rel="noreferrer" href="https://help.netflix.com/vi/contactus" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Liên hệ với chúng tôi
 									</a>
 								</li>
 								<li className="">
-									<NavLink to="/login" className="dark:hover:text-gray-200">
+									<NavLink to="/login" className={`dark:hover:text-gray-200 ${style.footer_item}`}>
 										Chỉ có trên Netflix
 									</NavLink>
 								</li>
@@ -119,7 +131,7 @@ function Footer() {
 					"
 				>
 					<span>© 2023 </span>
-					<a href="https://github.com/vulebaolong" className=" hover:text-gray-900 dark:hover:text-white transition">
+					<a href="https://github.com/vulebaolong" className={`hover:text-gray-900 dark:hover:text-white transition ${style.footer_item}`}>
 						VuLeBaoLong/NguyenThiHuynhNhi™
 					</a>
 					<span>. All Rights Reserved.</span>
@@ -134,22 +146,32 @@ function Footer() {
 						target="_blank"
 						rel="noreferrer"
 						href="https://www.facebook.com/profile.php?id=100073114167973"
-						className="text-gray-500 hover:text-gray-900 dark:hover:text-white transition"
+						className={`text-gray-500 hover:text-gray-900 dark:hover:text-white transition ${style.footer_item}`}
 					>
 						<FaFacebookF size={20} />
 					</a>
-					<a target="_blank" rel="noreferrer" href="https://github.com/vulebaolong" className="text-gray-500 hover:text-gray-900 dark:hover:text-white transition">
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="https://github.com/vulebaolong"
+						className={`text-gray-500 hover:text-gray-900 dark:hover:text-white transition ${style.footer_item}`}
+					>
 						<FaGithub size={20} />
 					</a>
 					<a
 						target="_blank"
 						rel="noreferrer"
 						href="https://www.instagram.com/vulebaolong/"
-						className="text-gray-500 hover:text-gray-900 dark:hover:text-white transition"
+						className={`text-gray-500 hover:text-gray-900 dark:hover:text-white transition ${style.footer_item}`}
 					>
 						<FaInstagram size={20} />
 					</a>
-					<a target="_blank" rel="noreferrer" href="mailto:vulebaolong@gmail.com" className="text-gray-500 hover:text-gray-900 dark:hover:text-white transition">
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="mailto:vulebaolong@gmail.com"
+						className={`text-gray-500 hover:text-gray-900 dark:hover:text-white transition ${style.footer_item}`}
+					>
 						<FaGoogle size={20} />
 					</a>
 				</div>
