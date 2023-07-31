606aebf89aba6fa99bbdecbda748f87b8a546930
vulebaolong <vulebaolong@gmail.com>
2023-07-31 22:34:03 +0700

    setting production

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import { ConfigProvider, theme } from "antd";
 import "./api/apiConfig.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.jsx";
+const isProduction = import.meta.env.PROD;
+if (isProduction) {
+	console.log = () => {};
+	console.error = () => {};
+	console.debug = () => {};
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ConfigProvider
