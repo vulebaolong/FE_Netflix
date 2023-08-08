d76c37037c33c903b1fda6581e00631cafb81a52
vulebaolong <vulebaolong@gmail.com>
2023-08-08 21:59:22 +0700

    add spec SwaggerUI

--- src/pages/ApiPage/SwaggerUIComponent.jsx
+++ src/pages/ApiPage/SwaggerUIComponent.jsx
@@ -1,12 +1,14 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import "./custom-swagger-theme.css";
+import swaggerJson from "./swagger.json";
 
 function SwaggerUIComponent() {
 	return (
 		<section className="pb-24">
 			<div className="container">
-				<SwaggerUI url=" http://localhost:3001/api/v1/QuanLySwagger/Swagger" />
+				{/* <SwaggerUI url=" http://localhost:3001/api/v1/QuanLySwagger/Swagger" /> */}
+				<SwaggerUI spec={swaggerJson} />
 			</div>
 		</section>
 	);
