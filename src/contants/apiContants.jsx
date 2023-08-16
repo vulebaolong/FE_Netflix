fe0e6b6e81bf3e0df1dfd0944c5378d9cb691d2d
vulebaolong <vulebaolong@gmail.com>
2023-08-16 13:40:02 +0700

    fix BASEURL

--- src/contants/apiContants.jsx
+++ src/contants/apiContants.jsx
@@ -16,8 +16,8 @@
 // trang tính bền vững (Sustainability): https://about.netflix.com/vi/sustainability
 
 // export const BASE_URL = 'https://movienew.cybersoft.edu.vn/api'
-// export const BASE_URL = 'https://movie-api-f6vr.onrender.com/api/v1'
-export const BASE_URL = 'http://localhost:3001/api/v1'
+export const BASE_URL = 'https://movie-api-f6vr.onrender.com/api/v1'
+// export const BASE_URL = 'http://localhost:3001/api/v1'
 export const TOKEN_CYBERSOFT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0NCIsIkhldEhhblN0cmluZyI6IjA5LzEyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTcwMjA4MDAwMDAwMCIsIm5iZiI6MTY3MjQxOTYwMCwiZXhwIjoxNzAyMjI3NjAwfQ.P5fJSMdFWDXkAXi_Hm7kZhuXoxo6xtTzIno_q6kp38I'
 // export const MANHOM = 'GP09'
 
